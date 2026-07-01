25ebe0279841829bbf96e237fc6b58e4d1994cd0
Zain Shaikh <skzain007995@gmail.com>
2026-07-01 22:32:38 +0530

    Initial vite setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-pink-300 p-10 text-center font-bold text-4xl text-white '>Gallery UI</h1>
+    </div>
+  )
+}
+
+export default App
