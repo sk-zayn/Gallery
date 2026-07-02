624dd935adc98c71023d4de186c101a19ce018e9
Zain Shaikh <skzain007995@gmail.com>
2026-07-02 23:54:26 +0530

    Loading data using useEffects and added buttons to go to next/prev pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
   const [userData, setUserData] = useState([]);
+  const [index, setIndex] = useState(1)
 
   const getData = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=3&limit=30"
+      `https://picsum.photos/v2/list?page=${index}&limit=24`,
     );
     setUserData(response.data);
     console.log(response.data);
   };
 
-  let printUserData = "Loading....";
+  useEffect(() => {
+      getData()
+  }, [index])
+
+
+  let printUserData =<h3 className=" text-center text-gray-300 text-sm">Loading....</h3>;
 
   if (userData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
-      return<div className="rounded-2xl  h-50 w-50">
-        <img className="h-full w-full object-cover rounded-2xl" src={elem.download_url} alt="" />
+      return (
+        <div key={idx}>
+          <a href={elem.url} target="_blank">
+            <div className="rounded-2xl overflow-hidden  h-46 w-52">
+            <img
+              className="h-full w-full object-cover "
+              src={elem.download_url}
+              alt=""
+            />
+          </div>
+          <h3 className="pt-2 pb-5 font-semibold text-center">{elem.author}</h3>
+          </a>
         </div>
-      ;
+      );
     });
   }
 
   return (
-    <div className="p-4 h-screen overflow-auto  bg-black text-white">
-      <button
-        className="bg-pink-400 p-4 active:scale-95 cursor-pointer rounded-2xl font-bold text-xl text-white"
-        onClick={getData}
-      >
-        Get data
-      </button>
-      <div className="flex flex-wrap gap-4 ">
-        {printUserData}
+    <div className="p-4 h-screen overflow-auto  bg-black text-white ">
+      <div className="flex flex-wrap gap-4 pl-11 pt-8">{printUserData}</div>
+      <div className="flex justify-center items-center gap-5 p-4">
+        <button className="bg-amber-400 text-black rounded px-4 py-2 text-sm cursor-pointer active:scale-95  font-semibold"
+        onClick={()=>{
+          if(index>1){
+            setIndex(index-1)
+          }
+
+        }}
+        >Prev</button>
+        <button className="bg-amber-400 text-black rounded px-4 py-2 text-sm cursor-pointer active:scale-95  font-semibold"
+        onClick={()=>{
+            setIndex(index+1)
+        }}
+        >Next</button>
       </div>
     </div>
   );
