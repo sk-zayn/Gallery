3e85a9f5fe5444f62d810fa324dc9079a21ccecb
Zain Shaikh <skzain007995@gmail.com>
2026-07-02 23:10:30 +0530

    Rendered images from API on button click using axios and  useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <div>
-      <h1 className='bg-pink-300 p-10 text-center font-bold text-4xl text-white '>Gallery UI</h1>
-    </div>
-  )
-}
+  const [userData, setUserData] = useState([]);
 
-export default App
+  const getData = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=3&limit=30"
+    );
+    setUserData(response.data);
+    console.log(response.data);
+  };
+
+  let printUserData = "Loading....";
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+      return<div className="rounded-2xl  h-50 w-50">
+        <img className="h-full w-full object-cover rounded-2xl" src={elem.download_url} alt="" />
+        </div>
+      ;
+    });
+  }
+
+  return (
+    <div className="p-4 h-screen overflow-auto  bg-black text-white">
+      <button
+        className="bg-pink-400 p-4 active:scale-95 cursor-pointer rounded-2xl font-bold text-xl text-white"
+        onClick={getData}
+      >
+        Get data
+      </button>
+      <div className="flex flex-wrap gap-4 ">
+        {printUserData}
+      </div>
+    </div>
+  );
+};
+
+export default App;
