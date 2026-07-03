8bfca68e35b895808d05b2835138e654dc7e6c11
Zain Shaikh <skzain007995@gmail.com>
2026-07-03 22:10:14 +0530

    Added page indicator between navigation buttons in App component

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,8 @@ const App = () => {
 
         }}
         >Prev</button>
+        <div>page {index}</div>
+
         <button className="bg-amber-400 text-black rounded px-4 py-2 text-sm cursor-pointer active:scale-95  font-semibold"
         onClick={()=>{
             setIndex(index+1)
